1592d035259c8e630c7973cbcba301bfb26a0a82
sergiovzambelli <sergiovzambelli@gmail.com>
2024-11-15 22:14:08 +0100

    Feature: metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,25 +11,19 @@ export default function Home() {
 
   return (
     <main className="relative min-h-dvh h-dvh flex flex-col justify-center items-center">
-      {/* <Image
-        src="/bg.jpg"
-        alt="Background"
-        layout="fill"
-        objectFit="cover"
-      /> */}
       <div className="relative z-10">
         {!showNo && (
           <button
             onClick={handleClick}
-            className="p-4 border border-black border-2 rounded-xl"
+            className="p-4 border-black border-2 rounded-xl text-xl xl:text-2xl"
           >
-            Is it Christmas???
+            Is it Christmas?
           </button>
         )}
         {showNo && (
           <div className="flex flex-col gap-2">
             <div className="text-9xl mt-4 font-bold">NO</div>
-            <button className="underline text-sm" onClick={() => setShowNo(false)}>Maybe it has changed...</button>
+            <button className="underline text-m" onClick={() => setShowNo(false)}>Maybe it has changed...</button>
           </div>
         )}
       </div>
